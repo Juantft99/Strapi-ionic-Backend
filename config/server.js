e79bfc06c9e082899e6bb12285e13c8282735640
Juan Tanquino <juanmehlol@gmail.com>
2021-07-19 23:23:26 -0500

    backend

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ad57b8844c395570e90cdc892668dca4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1dd5feec-754c-4ab2-bc4a-03b28c25034d'
+};
